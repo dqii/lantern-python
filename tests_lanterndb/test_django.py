c805ece410f0f3686e10f1b43dd4617d1a4e14d9
Di Qi <di@lantern.dev>
2023-12-29 20:17:17 -0800

    db credentials

--- tests_lanterndb/test_django.py
+++ tests_lanterndb/test_django.py
@@ -1,3 +1,4 @@
+import os
 import django
 from django.conf import settings
 from django.core import serializers
@@ -16,7 +17,11 @@ settings.configure(
     DATABASES={
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'postgres',
+            'NAME': os.environ.get('DB_NAME', 'postgres'),
+            'USER': os.environ.get('DB_USER', 'postgres'),
+            'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
+            'HOST': os.environ.get('DB_HOST', 'localhost'),
+            'PORT': os.environ.get('DB_PORT', '5432'),
         }
     }
 )

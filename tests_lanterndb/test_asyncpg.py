b0d17c67f996e28ad247fe832ff02d759fcfdc8b
Di Qi <di@lantern.dev>
2023-12-30 19:56:25 -0800

    fix

--- tests_lanterndb/test_asyncpg.py
+++ tests_lanterndb/test_asyncpg.py
@@ -1,3 +1,4 @@
+import os
 import asyncio
 import asyncpg
 import numpy as np
@@ -7,7 +8,13 @@ import pytest
 class TestAsyncpg:
     @pytest.mark.asyncio
     async def test_works(self):
-        conn = await asyncpg.connect(database='postgres')
+        conn = await asyncpg.connect(
+            user=os.environ.get('DB_USER', 'postgres'),
+            password=os.environ.get('DB_PASSWORD', 'postgres'),
+            database=os.environ.get('DB_NAME', 'postgres'),
+            host=os.environ.get('DB_HOST', 'localhost'),
+            port=os.environ.get('DB_PORT', '5432')
+        )
         await conn.execute('CREATE EXTENSION IF NOT EXISTS lantern')
         await conn.execute('DROP TABLE IF EXISTS items')
         await conn.execute('CREATE TABLE items (id SERIAL PRIMARY KEY, embedding real[3])')
@@ -25,10 +32,13 @@ class TestAsyncpg:
 
     @pytest.mark.asyncio
     async def test_pool(self):
-        async def init(conn):
-            await register_vector(conn)
-
-        pool = await asyncpg.create_pool(database='postgres', init=init)
+        pool = await asyncpg.create_pool(
+            user=os.environ.get('DB_USER', 'postgres'),
+            password=os.environ.get('DB_PASSWORD', 'postgres'),
+            database=os.environ.get('DB_NAME', 'postgres'),
+            host=os.environ.get('DB_HOST', 'localhost'),
+            port=os.environ.get('DB_PORT', '5432')
+        )
 
         async with pool.acquire() as conn:
             await conn.execute('CREATE EXTENSION IF NOT EXISTS lantern')

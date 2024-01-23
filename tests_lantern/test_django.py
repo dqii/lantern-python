78003d6c369938d0c3535c1426c02ec6d31f68c1
Di Qi <di@lantern.dev>
2024-01-23 14:30:14 -0800

    Add Lantern extras extension (#8)

--- tests_lantern/test_django.py
+++ tests_lantern/test_django.py
@@ -5,9 +5,8 @@ from django.core import serializers
 from django.db import connection, migrations, models
 from django.contrib.postgres.fields import ArrayField
 from django.db.migrations.loader import MigrationLoader
-from math import sqrt
 import numpy as np
-from lantern_django import LanternExtension, LanternExtrasExtension, HnswIndex, L2Distance, CosineDistance, RealField
+from lantern_django import LanternExtension, LanternExtrasExtension, HnswIndex, L2Distance, CosineDistance, RealField, TextEmbedding
 from unittest import mock
 
 settings.configure(
@@ -26,7 +25,7 @@ django.setup()
 
 
 class Item(models.Model):
-    embedding = ArrayField(RealField(), size=3, null=True)
+    embedding = ArrayField(RealField(), size=384, null=True)
 
     class Meta:
         app_label = 'myapp'
@@ -37,7 +36,7 @@ class Item(models.Model):
                 m=16,
                 ef=64,
                 ef_construction=64,
-                dim=3,
+                dim=384,
                 opclasses=['dist_l2sq_ops']
             )
         ]
@@ -51,12 +50,13 @@ class Migration(migrations.Migration):
 
     operations = [
         LanternExtension(),
+        LanternExtrasExtension(),
         migrations.CreateModel(
             name='Item',
             fields=[
                 ('id', models.BigAutoField(auto_created=True,
                  primary_key=True, serialize=False, verbose_name='ID')),
-                ('embedding', ArrayField(RealField(), size=3, null=True)),
+                ('embedding', ArrayField(RealField(), size=384, null=True)),
             ],
         ),
         migrations.AddIndex(
@@ -66,7 +66,7 @@ class Migration(migrations.Migration):
                 m=16,
                 ef=64,
                 ef_construction=64,
-                dim=3,
+                dim=384,
                 name='hnsw_idx',
                 opclasses=['dist_l2sq_ops']
             ),
@@ -91,7 +91,7 @@ def create_items():
         [1, 1, 2]
     ]
     for i, v in enumerate(vectors):
-        item = Item(id=i + 1, embedding=v)
+        item = Item(id=i + 1, embedding=v + [0] * 381)
         item.save()
 
 
@@ -100,24 +100,23 @@ class TestDjango:
         Item.objects.all().delete()
 
     def test_works(self):
-        item = Item(id=1, embedding=[1, 2, 3])
+        item = Item(id=1, embedding=[1, 2, 3] + [0] * 381)
         item.save()
         item = Item.objects.get(pk=1)
         assert item.id == 1
-        assert np.array_equal(np.array(item.embedding), np.array([1, 2, 3]))
+        assert np.array_equal(np.array(item.embedding),
+                              np.array([1, 2, 3] + [0] * 381))
 
-    def test_l2_distance(self):
+    def test_l2sq_distance(self):
         create_items()
-        distance = L2Distance('embedding', [1, 1, 1])
+        distance = L2Distance('embedding', [1, 1, 1] + [0] * 381)
         items = Item.objects.annotate(distance=distance).order_by(distance)
         assert [v.id for v in items] == [1, 3, 2]
-        # assert [v.distance for v in items] == [0, 1, sqrt(3)]
-        # TODO: Remove this and uncomment above use Euclidean distance instead of squared Euclidean distance
         assert [v.distance for v in items] == [0, 1, 3]
 
     def test_cosine_distance(self):
         create_items()
-        distance = CosineDistance('embedding', [1, 1, 1])
+        distance = CosineDistance('embedding', [1, 1, 1] + [0] * 381)
         items = Item.objects.annotate(distance=distance).order_by(distance)
         assert [v.id for v in items] == [1, 2, 3]
         # assert [v.distance for v in items] == [0, 0, 0.05719095841793653]
@@ -126,9 +125,17 @@ class TestDjango:
 
     def test_filter(self):
         create_items()
-        distance = L2Distance('embedding', [1, 1, 1])
+        distance = L2Distance('embedding', [1, 1, 1] + [0] * 381)
         items = Item.objects.alias(distance=distance).filter(distance__lt=1)
         assert [v.id for v in items] == [1]
+
+    def test_text_embedding(self):
+        create_items()
+        distance = L2Distance('embedding', TextEmbedding(
+            'BAAI/bge-small-en', 'hello'))
+        results = Item.objects.annotate(distance=distance).order_by('distance')
+        assert [v.id for v in results] == [1, 3, 2]
+        assert [v.distance for v in results] == [93.583, 95.45514, 103.85868]
 
     def test_serialization(self):
         create_items()
@@ -141,11 +148,11 @@ class TestDjango:
                     obj.save()
 
     def test_clean(self):
-        item = Item(id=1, embedding=[1, 2, 3])
+        item = Item(id=1, embedding=[1, 2, 3] + [0] * 381)
         item.full_clean()
 
     def test_get_or_create(self):
-        Item.objects.get_or_create(embedding=[1, 2, 3])
+        Item.objects.get_or_create(embedding=[1, 2, 3] + [0] * 381)
 
     def test_missing(self):
         Item().save()
